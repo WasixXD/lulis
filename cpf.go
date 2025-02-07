9ac384bc364877121cb11798bd9e5f2a2da3ba68
WasixXD <wasilewski2020@gmail.com>
2025-02-07 15:41:49 -0300

    ⚗️ Perf: testing

--- cpf.go
+++ cpf.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"os"
 	"strconv"
+	"sync"
 )
 
 func strings2ints(s []string) []int {
@@ -18,13 +20,12 @@ func strings2ints(s []string) []int {
 var cache map[int]int
 
 func ints2slice(s int) []int {
-	numberLen := len(strconv.Itoa(s))
+	numberLen := 9
 	tmp := make([]int, numberLen)
-	for c := 0; c < numberLen; c++ {
-		tmp[numberLen-c-1] = s % 10
-		s /= 10
+	for c := 0; c < numberLen && s > 0; c++ {
+		tmp[numberLen-1-c] = s % 10
+		s = int(s / 10)
 	}
-
 	return tmp
 }
 
@@ -38,60 +39,69 @@ func sum(s []int) (sum int) {
 }
 
 func ints2byte(s []int) []byte {
-	tmp := make([]byte, len(s)+1)
+	tmp := make([]byte, len(s))
 	for i, v := range s {
 		tmp[i] = byte(v + '0')
 	}
 	return tmp
 }
 
-func genCpfs(start int, end int, file string) {
-	handle, _ := os.OpenFile(file, os.O_WRONLY, os.ModeAppend)
-
-	// var thisList strings.Builder
+func genCpfs(start int, end int, comm chan []int) {
 
 	for n := start; n <= end; n++ {
 		nums := ints2slice(n)
+
 		value := 0
+
 		for i, n := range nums {
 			value += (10 - i) * n
 		}
 
 		digit1 := (11 - (value % 11)) % 10
-
 		value2 := value + (sum(nums) + (digit1 * 2))
 		digit2 := (11 - (value2 % 11)) % 10
+		nums = append(nums, digit1, digit2)
 
-		nums = append(nums, digit1)
-		nums = append(nums, digit2)
-		handle.Write(ints2byte(nums))
+		comm <- nums
 	}
+
+}
+
+func minimun(a, b int64) int64 {
+	if a < b {
+		return a
+	}
+	return b
 }
 
 func main() {
 	// cpfsTotal := int64(10e11)
-	// nCpus := runtime.NumCPU()
+	cpfsTotal := int64(10e8) // 0.1%
+	wait := sync.WaitGroup{}
+	nCpus := int64(12)
 
-	// cache = make(map[int]int)
-	genCpfs(100000000, 200000000, "cpfs.txt")
-	// nums := strings2ints(strings.Split(CPF[0:len(CPF)-2], ""))
+	c := make(chan []int)
 
-	// digit1 := 11 - (value % 11)
-	// digit2 := 0
-	// if digit1 < 10 {
-	// 	nums = append(nums, digit1)
-	// 	value = 0
-	// 	for i, n := range nums {
-	// 		mult := (11 - i)
-	// 		value += mult * n
-	// 	}
-	// 	digit2 = 11 - (value % 11)
-	// 	if digit2 >= 10 {
-	// 		digit2 = 0
-	// 	}
-	// }
-	// nums = append(nums, digit2)
+	go func() {
+		for v := range c {
+			fmt.Println(v)
+		}
+	}()
 
-	// fmt.Println(strings.Trim(strings.Join(strings.Fields(fmt.Sprint(nums)), ""), "[]"))
+	for n := range nCpus {
+		// division of labor
+		amount := cpfsTotal / nCpus
+		start := (n + 1) * amount
+		end := minimun(start+amount, cpfsTotal)
+		cpfsTotal -= amount
 
+		wait.Add(1)
+		go func(start int, end int) {
+			defer wait.Done()
+			genCpfs(start, end, c)
+		}(int(start), int(end))
+	}
+
+	wait.Wait()
+	os.Exit(0)
 }
