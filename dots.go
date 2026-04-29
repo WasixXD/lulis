09a00690a809c14ade9cd8e0632ca56df4b0efc0
WasixXD <wasilewski2020@gmail.com>
2026-04-28 22:15:51 -0300

    some kinda of dot product

---
+++ dots.go
@@ -0,0 +1,106 @@
+package main
+
+var weights = [9]int{10, 9, 8, 7, 6, 5, 4, 3, 2}
+
+func DotNaive(a [BATCH_SIZE][9]int, b [9]int) int {
+	sum := 0
+
+	for i, x := 0, 0; i < 9 && x < BATCH_SIZE; i, x = i+1, x+1 {
+		sum += a[x][i] * b[i]
+	}
+
+	return sum
+}
+
+func DotUnroll(a [BATCH_SIZE][9]int, b [9]int) int {
+	sum := 0
+
+	for x := 0; x < BATCH_SIZE; x++ {
+		sum +=
+			a[x][0]*b[0] +
+				a[x][1]*b[1] +
+				a[x][2]*b[2] +
+
+				a[x][3]*b[3] +
+				a[x][4]*b[4] +
+				a[x][5]*b[5] +
+
+				a[x][6]*b[6] +
+				a[x][7]*b[7] +
+				a[x][8]*b[8]
+	}
+
+	return sum
+}
+func DotBCE(a [BATCH_SIZE][9]int, b [9]int) int {
+	sum := 0
+
+	for x := 0; x < BATCH_SIZE; x++ {
+		for i := 0; i < 9; i += 3 {
+			aTmp := a[x][i : i+3 : i+3]
+			bTmp := b[i : i+3 : i+3]
+
+			sum +=
+				aTmp[0]*bTmp[0] +
+					aTmp[1]*bTmp[1] +
+					aTmp[2]*bTmp[2]
+		}
+	}
+
+	return sum
+}
+func DotFullUnroll(a [4][9]int, b [9]int) int {
+	return (a[0][0]*b[0] +
+		a[0][1]*b[1] +
+		a[0][2]*b[2] +
+		a[0][3]*b[3] +
+		a[0][4]*b[4] +
+		a[0][5]*b[5] +
+		a[0][6]*b[6] +
+		a[0][7]*b[7] +
+		a[0][8]*b[8] +
+
+		a[1][0]*b[0] +
+		a[1][1]*b[1] +
+		a[1][2]*b[2] +
+		a[1][3]*b[3] +
+		a[1][4]*b[4] +
+		a[1][5]*b[5] +
+		a[1][6]*b[6] +
+		a[1][7]*b[7] +
+		a[1][8]*b[8] +
+
+		a[2][0]*b[0] +
+		a[2][1]*b[1] +
+		a[2][2]*b[2] +
+		a[2][3]*b[3] +
+		a[2][4]*b[4] +
+		a[2][5]*b[5] +
+		a[2][6]*b[6] +
+		a[2][7]*b[7] +
+		a[2][8]*b[8] +
+
+		a[3][0]*b[0] +
+		a[3][1]*b[1] +
+		a[3][2]*b[2] +
+		a[3][3]*b[3] +
+		a[3][4]*b[4] +
+		a[3][5]*b[5] +
+		a[3][6]*b[6] +
+		a[3][7]*b[7] +
+		a[3][8]*b[8])
+}
+
+func Dot4(a [4][9]int, b [9]int) (int, int, int, int) {
+	var s0, s1, s2, s3 int
+
+	for i := 0; i < 9; i++ {
+		w := b[i]
+		s0 += a[0][i] * w
+		s1 += a[1][i] * w
+		s2 += a[2][i] * w
+		s3 += a[3][i] * w
+	}
+
+	return s0, s1, s2, s3
+}
--- main.go
+++ main.go
@@ -8,35 +8,81 @@ import (
 	"time"
 )
 
-func calc2Digits(digits *[9]int) []string {
+const (
+	CPF_DIGITS = 9
+	BATCH_SIZE = 4
+)
 
-	sum := 0
+func calcCpfRange(start, end int) int {
+	var digits [CPF_DIGITS]int
+	var local int
 
-	// Holds A+B+C+D+E+F+G+H+I
-	baseSum := 0
-	for i, j := 0, 10; i < 9; i, j = i+1, j-1 {
-		digit := digits[i]
-		sum += digit * j
-		baseSum += digit
+	for k := start; k <= end; k += BATCH_SIZE {
+		var batchCpf [BATCH_SIZE][CPF_DIGITS]int
+		for x := 0; x < BATCH_SIZE; x++ {
+			// Instead of creating an array every loop
+			// we use just one and increments its bits one by one
+			// 0 0 0 0 0 0 0 0 1
+			// 0 0 0 0 0 0 0 0 2
+			// ...
+			// 0 0 0 0 0 0 0 1 0
+			for i := 8; i >= 0; i-- {
+				digits[i]++
+				if digits[i] < 10 {
+					break
+				}
+				digits[i] = 0
+			}
+			batchCpf[x] = digits
+		}
+
+		// Holds A+B+C+D+E+F+G+H+I
+		baseSum := [BATCH_SIZE]int{}
+		sum := [BATCH_SIZE]int{}
+
+		for i, j := 0, 10; i < 9; i, j = i+1, j-1 {
+			// Loop unrolling
+			digit := batchCpf[0][i]
+			sum[0] += digit * j
+			baseSum[0] += digit
+
+			digit = batchCpf[1][i]
+			sum[1] += digit * j
+			baseSum[1] += digit
+
+			digit = batchCpf[2][i]
+			sum[2] += digit * j
+			baseSum[2] += digit
+
+			digit = batchCpf[3][i]
+			sum[3] += digit * j
+			baseSum[3] += digit
+		}
+
+		var finalDigits [BATCH_SIZE][2]int
+		for x := 0; x < BATCH_SIZE; x++ {
+			// digit1 is (10A + 9B + 8C + 7D + 6E + 5F + 4G + 3H + 2I) % 11
+			rem := sum[x] % 11
+			digit1 := remTable[rem]
+
+			// digit2 is 11A + 10B + 9C + 8D + 7E + 6F + 5G + 4H + 3I + 2J
+			// but can also be written as:
+			// (10A + 9B + 8C + 7D + 6E + 5F + 4G + 3H + 2I) + (A+B+C+D+E+F+G+H+I) + 2 * digit1
+			// we save one loop
+			sum[x] += baseSum[x] + 2*digit1
+
+			// its has come a time that the division is one of the most
+			// expensive operations on this algorithm. Unfortunely even
+			// compiler optimization wont save us
+			rem = sum[x] % 11
+			digit2 := remTable[rem]
+
+			finalDigits[x] = [2]int{digit1, digit2}
+			local += digit1 + digit2
+		}
 	}
 
-	// digit1 is (10A + 9B + 8C + 7D + 6E + 5F + 4G + 3H + 2I) % 11
-	rem := sum % 11
-	digit1 := remTable[rem]
-
-	// digit2 is 11A + 10B + 9C + 8D + 7E + 6F + 5G + 4H + 3I + 2J
-	// but can also be written as:
-	// (10A + 9B + 8C + 7D + 6E + 5F + 4G + 3H + 2I) + (A+B+C+D+E+F+G+H+I) + 2 * digit1
-	// we save one loop
-	sum += baseSum + 2*digit1
-
-	rem = sum % 11
-	digit2 := remTable[rem]
-
-	_, _ = digit1, digit2
-	// fmt.Println(digits, digit1, digit2)
-
-	return []string{}
+	return local
 }
 
 func main() {
@@ -46,27 +92,8 @@ func main() {
 		log.Fatal(err)
 	}
 
-	var digits [9]int
 	now := time.Now()
-	for i := 1; i < 999_999_999; i++ {
-
-		// Instead of creating an array every loop
-		// we use just one and increments its bits one by one
-		// 0 0 0 0 0 0 0 0 1
-		// 0 0 0 0 0 0 0 0 2
-		// ...
-		// 0 0 0 0 0 0 0 1 0
-		for i := 8; i >= 0; i-- {
-			digits[i]++
-			if digits[i] < 10 {
-				break
-			}
-			digits[i] = 0
-		}
-
-		// let pass a pointer to not copy the slice
-		calc2Digits(&digits)
-	}
+	calcCpfRange(0, 100_000_000)
 
 	pprof.StopCPUProfile()
 	f.Close()
